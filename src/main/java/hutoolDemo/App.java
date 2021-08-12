d2697ecc8d833c7261c8484b895107c1d990f3a0
lishuai <lishuai0214@gmail.com>
2021-08-12 11:25:55 +0800

    init

---
+++ src/main/java/hutoolDemo/App.java
@@ -0,0 +1,13 @@
+package hutoolDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
